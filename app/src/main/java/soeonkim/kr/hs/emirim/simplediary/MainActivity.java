49120eda2d4ea80908a77e62825382fec98f5c89
Kimsoeon <a1thdjs@naver.com>
2017-05-30 20:47:07 +0900

    #3 Current date and DatePicker init

--- app/src/main/java/soeonkim/kr/hs/emirim/simplediary/MainActivity.java
+++ app/src/main/java/soeonkim/kr/hs/emirim/simplediary/MainActivity.java
@@ -6,10 +6,14 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     DatePicker date;
     EditText edit;
     Button but;
+    String fileName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
         date = (DatePicker)findViewById(R.id.date_pick);
         edit = (EditText)findViewById(R.id.edit);
         but = (Button)findViewById(R.id.but);
+        //Calendar는 추상클래스. 바로 객체생성 불가. 그렇다고 상속받아서 사용하긴 귀찮. getInstance 메서드가 (현재객체 참조값 반환하도록)만들어져있음.
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DATE); //DAY OF MONTH랑 같음
+
+        date.init(year, month, day, new DatePicker.OnDateChangedListener() { //네번째 매개변수는 날짜가변경됐을때 동작할 핸들러
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+                fileName += year + " / " + (month+1) + " / " + day + ".txt";
+                String readData = readDiary(fileName);
+                edit.setText(readData);
+                but.setEnabled(true);
+            }
+        });
+    }//onCreate 닫음
+    public String readDiary(String fileName){
+        return null;
     }
 }
